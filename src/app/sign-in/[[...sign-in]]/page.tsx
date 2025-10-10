4861252ac3cd21188c436f8ed81962bce8deea66
Your Name <ayshhaaaa00@gmail.comgit config --global user.name ayshald>
2025-10-11 00:42:22 +0400

    updated-schema

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
   return (
     <div className="flex min-h-[70vh] items-center justify-center p-6">
-      <SignIn afterSignInUrl="/dashboard" afterSignUpUrl="/dashboard"/>
+      <SignIn/>
     </div>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function Page() {
   return (
     <div className="flex min-h-[70vh] items-center justify-center p-6">
-      <SignUp redirectUrl="/dashboard" />
+      <SignUp/>
     </div>
   );
 }
